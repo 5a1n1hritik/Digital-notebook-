dee27bd556e1b15066c5826c3ccb67b666f7472d
5a1n1hritik <sainihritik033@gmail.com>
2024-02-25 19:03:13 +0530

    Second commit

--- frontend/inotebook/src/App.js
+++ frontend/inotebook/src/App.js
@@ -1,11 +1,27 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
+import Navbar from "./components/Navbar";
+import HomePage from "./components/Home";
+import AboutPage from "./components/About";
+import NoteState from "./context/NoteState";
 
 function App() {
   return (
-    <div className="App">
-      <h1>this is digital notebook</h1>
-    </div>
+    <>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+          <Routes>
+            {/* <Route exact path="/" element={<Navbar />} /> */}
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/about" element={<AboutPage />} />
+          </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
+    </>
   );
 }
 
